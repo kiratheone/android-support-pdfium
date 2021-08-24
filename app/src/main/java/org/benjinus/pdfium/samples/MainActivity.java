81ccef429a2f3abce9989337c39f91f847e1fe5e
Arik Achmad <arikarik.ae@gmail.com>
2021-08-24 08:57:49 +0700

    test search usage

--- app/src/main/java/org/benjinus/pdfium/samples/MainActivity.java
+++ app/src/main/java/org/benjinus/pdfium/samples/MainActivity.java
@@ -6,27 +6,40 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.util.Log;
 import android.widget.ImageView;
+
 import java.io.File;
+
 import org.benjinus.pdfium.Meta;
 import org.benjinus.pdfium.PdfiumSDK;
+import org.benjinus.pdfium.search.TextSearchContext;
 import org.benjinus.pdfium.util.Size;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView mImageView;
+    PdfiumSDK mSdk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mImageView = findViewById(R.id.imageView);
+        mSdk = new PdfiumSDK();
+        int width = getScreenWidth();
+        int height = getScreenHeight();
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
@@ -35,6 +48,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         decodePDFPage();
+        findViewById(R.id.button).setOnClickListener((view) -> {
+            TextSearchContext search = mSdk.newPageSearch(0, "Parameters", false, false);
+            RectF location = search.searchNext();
+            RectF location2 = mSdk.measureCharacterBox(0, 8);
+            Log.d("PDFIUM", "Rect: " + mSdk.extractText(0, location2) );
+            Log.d("PDFIUM", "Count: " + search.countResult() );
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            mSdk.renderPageBitmap(bitmap, 0, 0, 0, width, height, true);
+            Bitmap findTextBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            int color = 0xff424242;
+            Paint paint = new Paint();
+            Canvas canvas = new Canvas(findTextBitmap);
+
+            paint.setColor(color);
+            canvas.drawBitmap(bitmap, new Matrix(), new Paint());
+            canvas.drawRect(location, paint);
+            mImageView.setImageBitmap(findTextBitmap);
+        });
     }
 
     private void decodePDFPage() {
@@ -44,28 +75,27 @@ public class MainActivity extends AppCompatActivity {
 
             ParcelFileDescriptor fileDescriptor = ParcelFileDescriptor.open(pdfFile, MODE_READ_ONLY);
 
-            PdfiumSDK sdk = new PdfiumSDK();
-            sdk.newDocument(fileDescriptor);
 
-            Log.d("PDFIUM", "Page count: " + sdk.getPageCount());
+            mSdk.newDocument(fileDescriptor);
 
-            Meta meta = sdk.getDocumentMeta();
+            Log.d("PDFIUM", "Page count: " + mSdk.getPageCount());
+
+            Meta meta = mSdk.getDocumentMeta();
             Log.d("PDFIUM", meta.toString());
 
-            sdk.openPage(0);
+            mSdk.openPage(0);
 
-            Size size = sdk.getPageSize(0);
+            Size size = mSdk.getPageSize(0);
             Log.d("PDFIUM", "Page size: " + size.toString());
 
             int width = getScreenWidth();
             int height = getScreenHeight();
 
             Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-            sdk.renderPageBitmap(bitmap, 0, 0, 0, width, height, true);
+            mSdk.renderPageBitmap(bitmap, 0, 0, 0, width, height, true);
 
             mImageView.setImageBitmap(bitmap);
 
-            sdk.closeDocument();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -80,4 +110,9 @@ public class MainActivity extends AppCompatActivity {
         return Resources.getSystem().getDisplayMetrics().widthPixels;
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSdk.closeDocument();
+    }
 }
